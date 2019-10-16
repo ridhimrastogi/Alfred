b3656618fa076b092a07e5fb7822dad02d9b4dae
jaymin.desai <jaymindesai766@gmail.com>
2019-10-15 23:37:12 -0400

    mattermost client tweaks and first interaction with alfred

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const Client = require('./mattermost-client/client');
+const fs = require('fs')
+
+let host = "alfred-filebot.herokuapp.com"
+let group = "alfred"
+let bot_name = "alfred";
+let client = new Client(host, group, {});
+
+async function main() {
+    let request = await client.tokenLogin(process.env.BOTTOKEN);
+
+    client.on('message', function (msg) {
+        console.log(msg);
+        if (hears(msg, "design")) {
+            parseMessage(msg);
+        }
+    });
+
+}
+
+function hears(msg, text) {
+    if (msg.data.sender_name == bot_name) return false;
+    if (msg.data.post) {
+        let post = JSON.parse(msg.data.post);
+        if (post.message.indexOf(text) > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+async function parseMessage(msg) {
+    let channel = msg.broadcast.channel_id;
+    client.postMessage("Uploading DESIGN.md", channel);
+    data = fs.createReadStream('../DESIGN.md');
+    client.uploadFile(channel, data, (res) => {
+        files = res.file_infos.map(x => x.id);
+        msg = {
+            message: "PFA, my internals!",
+            file_ids: files,
+        }
+        client.postMessage(msg, channel);
+    });
+}
+
+(async () => {
+    await main();
+})()
