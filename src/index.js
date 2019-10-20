cb16ac3a7d9cd96a559cd916a954b933d87f21a0
Shubham Pampattiwar <sdpampat@ncsu.edu>
2019-10-20 15:08:26 -0400

    Integrated API calls for list and create

--- src/index.js
+++ src/index.js
@@ -43,7 +43,6 @@ async function main() {
     let request = await client.tokenLogin(process.env.BOTTOKEN);
 
     client.on('message', function (msg) {
-        //console.log(msg.data.post);
         if (hears(msg, "design")) {
             parseMessage(msg);
         }
@@ -52,6 +51,9 @@ async function main() {
         }
         else if(hears(msg,"delete")){
             deleteFile(msg);
+        }
+        else if(hears(msg,"list")){
+            listFiles(msg);
         }
     });
 
@@ -68,20 +70,33 @@ function hears(msg, text) {
     return false;
 }
 
-function createFile(msg){
+//stub for listing drive files
+async function listFiles(msg){
+    let res = await drive.getFiles();
+    let files = res.files;
+    let fileNames = new Array();
+    for(n in files){
+        fileNames.push(files[n].name + "\n");
+    }
+    let channel = msg.broadcast.channel_id;
+    client.postMessage("Following are the files present in your drive: \n" + fileNames.join(''),channel);
+}
+
+//stub for creating a file 
+async function createFile(msg){
     let post = JSON.parse(msg.data.post);
     let fileName = post.message.split(" ").filter(x => x.includes('.'))[0]
     let fileExtension = fileName.split(".")[1]
-
     let createFileObj = {
         "originalFilename" : fileName,
         "mimeType" : getMIMEType(fileExtension)
     }
-
+    let res = await drive.createFile(createFileObj);
     let channel = msg.broadcast.channel_id;
-    client.postMessage("Creating file with object " + JSON.stringify(createFileObj,null,2),channel);
+    client.postMessage("Created file " + fileName + " successfully\n" + "Here is the link for the same: " + res.webViewLink ,channel);
 }
 
+//function to get the MIME type of a particular file
 function getMIMEType(fileExtension){
     if(fileExtension == "doc"){
         return "application/msword"
@@ -106,6 +121,7 @@ function getMIMEType(fileExtension){
     }
 }
 
+//stub to delete a file
 function deleteFile(msg){
     let post = JSON.parse(msg.data.post);
     let fileName = post.message.split(" ").filter(x => x.includes('.'))[0]
