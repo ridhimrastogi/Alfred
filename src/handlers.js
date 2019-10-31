fe51868267d2646d61d277331d2d27d15f5765da
Jaymin Desai <jddesai2@ncsu.edu>
2019-10-31 18:37:35 -0400

    project restructirung

---
+++ src/handlers.js
@@ -0,0 +1,148 @@
+const drive = require("./drive.js");
+const helper = require("./utils/helpers.js");
+
+//stub for listing drive files
+async function listFiles(msg, client) {
+
+    let res = await drive.getFiles(),
+        files = res.files,
+        fileNames = new Array();
+
+    for (n in files) {
+        fileNames.push(files[n].name + "\n");
+    }
+
+    let channel = msg.broadcast.channel_id;
+    client.postMessage("Following are the files present in your drive: \n" + fileNames.join(''), channel);
+}
+
+//stub for creating a file 
+async function createFile(msg, client) {
+
+    let channel = msg.broadcast.channel_id,
+        post = JSON.parse(msg.data.post),
+        fileName = post.message.split(" ").filter(x => x.includes('.'))[0];
+
+    // TODO: Common stub. Needs to be extracted.
+    if (!helper.checkValidFile(fileName))
+        return client.postMessage("Please Enter a valid file name", channel);
+
+    let fileExtension = fileName.split(".")[1];
+
+    if (!helper.checkValidFileExtension(fileExtension))
+        return client.postMessage("Please enter a supported file extension.\n" +
+            "Supported file extenstion: doc, docx, ppt, pptx, xls, xlsx, pdf", channel);
+
+    let res = await drive.createFile({
+        "originalFilename": fileName,
+        "mimeType": helper.getMIMEType(fileExtension)
+    }),
+        fileLink = res.webViewLink,
+        usernames = post.message.split(" ").filter(x => x.includes('@') && x !== "@alfred").map(uh => uh.replace('@', ''));
+
+    sendDirecMessageToUsers(usernames, fileName, fileLink, channel);
+    client.postMessage("Created file " + fileName + " successfully\n" + "Here is the link for the same: " + fileLink, channel);
+}
+
+//function to download file
+async function downloadFile(msg, client) {
+    let channel = msg.broadcast.channel_id;
+    let post = JSON.parse(msg.data.post);
+
+    let fileName = post.message.split(" ").filter(x => x.includes('.'))[0];
+
+    // TODO: Common stub. Needs to be extracted.
+    if (!helper.checkValidFile(fileName))
+        return client.postMessage("Please Enter a valid file name", channel);
+
+    let fileExtension = fileName.split(".")[1];
+
+    if (!helper.checkValidFileExtension(fileExtension))
+        return client.postMessage("Please enter a supported file extension.\n" +
+            "Supported file extenstion: doc, docx, ppt, pptx, xls, xlsx, pdf", channel);
+
+    let res = await drive.getFiles();
+    let files = res.files;
+    let file = files.find(function (element) {
+        return element.name == fileName;
+    });
+
+    if (typeof file === 'undefined') {
+        return client.postMessage("No such file found!", channel);
+    }
+
+    let result = await drive.downloadAFile(file.name)
+    return client.postMessage("Download link: " + result.webViewLink, channel);
+}
+
+/*
+    Sample query: @alfred add @ridhim @shubham as collaborators with read and edit access in file.doc
+    Sample query: @alfred change/update @ridhim access to read access in file.doc
+*/
+async function updateCollaboratorsInFile(msg, client) {
+
+    let channel = msg.broadcast.channel_id,
+        message = JSON.parse(msg.data.post).message,
+        splittedMessageBySpace = message.split(" ");
+
+    let fileName = splittedMessageBySpace.filter(x => x.includes('.'))[0],
+        collaboatorList = splittedMessageBySpace.filter(x => x.includes('@') && x !== "@alfred");
+    permissionList = splittedMessageBySpace
+        .filter(x => ["read", "edit", "comment"].includes(x.toLowerCase()))
+        .map(x => x.toLowerCase());
+
+    if (collaboatorList.length !== permissionList.length)
+        return client.postMessage("Invalid request!", channel);
+
+    // TODO: Common stub. Needs to be extracted.
+    if (!helper.checkValidFile(fileName))
+        return client.postMessage("Please Enter a valid file name", channel);
+
+    if (!helper.checkValidFileExtension(fileName.split(".")[1]))
+        return client.postMessage("Please enter a supported file extension.\n" +
+            "Supported file extenstion: doc, docx, ppt, pptx, xls, xlsx, pdf", channel);
+
+    let file = await drive.getAFile(fileName),
+        usernames = collaboatorList.map(uh => uh.replace('@', ''));
+
+    if (file === undefined)
+        return client.postMessage("No such file found!", channel);
+
+    file.sharingUser = collaboatorList;
+    file.permissions = permissionList;
+
+    let res = await drive.updateFile(file),
+        fileLink = res.webViewLink;
+
+    if (fileLink !== undefined) {
+
+        sendDirecMessageToUsers(usernames, fileName, fileLink, channel);
+        client.postMessage("Updated collaborators to file " + fileName + " successfully\n" + "Here is the link for the same: " + fileLink, channel);
+
+    } else {
+        return client.postMessage("Error occurred while adding collaborators.!! :(", channel);
+    }
+}
+
+//function to DM users
+function sendDirecMessageToUsers(usernames, fileName, fileLink, client) {
+    userIDS = usernames.map(username => client.getUserIDByUsername(username));
+    for (userID in userIDS) {
+        client.getUserDirectMessageChannel(
+            userIDS[userID],
+            fileName,
+            fileLink,
+            (fileName, fileLink, channel) => {
+                client.postMessage("You have been added as a collaborator for " + fileName + "\n" + "Here is the link for the same: " + fileLink, channel.id)
+            }
+        );
+    }
+}
+
+module.exports = {
+    listFiles,
+    createFile,
+    downloadFile,
+    updateCollaboratorsInFile,
+    sendDirecMessageToUsers
+};
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,37 +1,3 @@
-const data = require("./mock.json")
-const nock = require('nock')
-
-// Scope for listing files
-const scope1 = nock('https://www.googleapis.com/drive/v3/files')
-    .persist()
-    .get('/')
-    .reply(200, JSON.stringify(data.fileList));
-
-// Scope for creating a file
-const scope2 = nock('https://www.googleapis.com/drive/v3/files')
-    .persist()
-    .post('/')
-    .reply(201, JSON.stringify(data.file));
-
-// Scope for fetching a file
-const scope3 = nock('https://www.googleapis.com/drive/v3/files')
-    .persist()
-    .get(/[a-z0-9]+$/)
-    .reply(200, JSON.stringify(data.file));
-
-// Scope for downloading a file
-const scope4 = nock('https://www.googleapis.com/drive/v3/files')
-    .persist()
-    .get(uri => uri.includes('?alt=media'))
-    .reply(200, JSON.stringify(data.file));
-
-// Scope for updating a file
-const scope5 = nock('https://www.googleapis.com/drive/v3/files')
-    .persist()
-    .patch(/[a-z0-9]+$/)
-    .reply(200, JSON.stringify(data.file));
-
-
 //function to get the MIME type of a particular file
 function getMIMEType(fileExtension) {
     let type = null;
@@ -70,12 +36,10 @@ function getMIMEType(fileExtension) {
 }
 
 function checkValidFile(fileName) {
-
     return !(fileName === undefined || fileName.split(".")[0].length == 0);
 }
 
 function checkValidFileExtension(fileExtension) {
-
     return !(fileExtension === undefined || getMIMEType(fileExtension) == null)
 }
 
